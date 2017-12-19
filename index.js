9d9c926ba767adb5c1edf40b46954b4adb0063fc
jiherrero4 <jiherrero4@gmail.com>
2017-12-19 18:14:55 +0100

    luis

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ server.post('/', connector.listen());
 var bot = new builder.UniversalBot(connector);
 
 
-/*  ************************************************************** LUIS MODEL ******************************************************************************************
+
 
 // Make sure you add code to validate these fields
 //var luisAppId = process.env.LuisAppId;
@@ -45,7 +45,7 @@ var bot = new builder.UniversalBot(connector);
 
 //const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
 
-
+// ************************************************************** LUIS MODEL ******************************************************************************************
 
 const LuisModelUrl = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/6a13aa8f-1ae3-4ceb-baff-1ecb4a98726e?subscription-key=7a8cc728767249589b45b66f34adcdcf&timezoneOffset=0&verbose=true&q=';
 
@@ -53,40 +53,12 @@ const LuisModelUrl = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/
 // Main dialog with LUIS
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
 var intents = new builder.IntentDialog({ recognizers: [recognizer] })
+   
     
-    //.matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
-    
-    .matches('Saludo', (session, args) => {
-        session.send('greet found by luis');
-        console.log(args);
-    })
-    .matches('Test', (session, args) => {
-        session.send('probando');
-        var url = 'https://docs.microsoft.com/en-us/bot-framework/media/how-it-works/architecture-resize.png';
-        sendInternetUrl(session, url, 'image/png', 'BotFrameworkOverview.png');
-        console.log(args);
-    })
 
-    .onDefault((session) => {
-        console.log('PASO1');
-        session.send('Sorry, I did not understand \'%s\'.', session.message.text);
-    });
 
-// Sends attachment using an Internet url
-function sendInternetUrl(session, url, contentType, attachmentFileName) {
-    var msg = new builder.Message(session)
-        .addAttachment({
-            contentUrl: url,
-            contentType: contentType,
-            name: attachmentFileName
-        });
-
-    session.send(msg);
-}
-
-bot.dialog('/', intents);
-*/
 //*********************************************************************** API AI MODEL ***************************************************************************
+/*
 var intents = new builder.IntentDialog({
     recognizers: [
         apiaiRecognizer
@@ -94,6 +66,8 @@ var intents = new builder.IntentDialog({
     intentThreshold: 0.2,
     recognizeOrder: builder.RecognizeOrder.series
 });
+
+*/
 
 intents.matches('salas.tipologia', '/salas.tipologia');
 intents.matches('peticion.preventa', '/peticion.preventa');
